468a1f0dd7a7d7e8050e7ad5958f094cadd4b951
kppantino <kppantino@student.apc.edu.ph>
2024-12-16 16:27:02 +0800

    DOM EDIT

--- home/js/myScript.js
+++ home/js/myScript.js
@@ -4,9 +4,9 @@ function myFunction() {
 
   
   function myFunction2() {
-    const element = document.getElementsByTagName("div");
+    const element = document.getElementsByTagName("body");
 
-    document.getElementById("demo").innerHTML = '<br> The text in first paragraph (index 0) is: <br>'
+    document.getElementById("demo").innerHTML = '<br> FUnction 2 Trial :))) : <br>'
     + element[0].innerHTML;
     
   }
