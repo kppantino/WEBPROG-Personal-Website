2aa961dbf96dac40a6c7b7d5de00446d99ec89d3
kppantino <kppantino@student.apc.edu.ph>
2024-12-16 16:17:16 +0800

    DOM edit

---
+++ home/js/myScript.js
@@ -0,0 +1,12 @@
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Paragraph changed.";
+  }
+
+  
+  function myFunction2() {
+    const element = document.getElementsByTagName("div");
+
+    document.getElementById("demo").innerHTML = '<br> The text in first paragraph (index 0) is: <br>'
+    + element[0].innerHTML;
+    
+  }
